a6207f3d9c3c1d05dac1e607ca4853996b838b13
Tony Immanuel Richard Clint <immanuelrichard72@gmail.com>
2019-09-06 14:33:10 +0700

    Update recipes.page.ts
    tampilan bisa hilang setelah dihapus

--- week-02/src/app/recipes/recipes.page.ts
+++ week-02/src/app/recipes/recipes.page.ts
@@ -14,6 +14,10 @@ export class RecipesPage implements OnInit {
   constructor(private recipesService: RecipesService) { }
 
   ngOnInit() {
+    // this.recipes = this.recipesService.getAllRecipes();
+  }
+
+  ionViewWillEnter(){
     this.recipes = this.recipesService.getAllRecipes();
   }
 
