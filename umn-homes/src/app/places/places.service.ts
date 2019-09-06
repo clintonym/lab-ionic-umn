dfc693f4462f4b12e2673cd383b9b6110ddbe362
Tony Immanuel Richard Clint <immanuelrichard72@gmail.com>
2019-09-06 17:21:14 +0700

    Week04_umn-homes

---
+++ umn-homes/src/app/places/places.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Place } from './place.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlacesService {
+
+  private _places: Place[] = [
+    new Place(
+      'p1',
+      'Gading Apartment',
+      '2BR, Luas dan Cozy',
+      'https://origin.pegipegi.com/jalan/images/pict1L/Y8/Y953168/Y953168051.jpg',
+      100000000
+    ),
+    new Place(
+      'p2',
+      'Serpong Apartment',
+      'Apartemen Romantis',
+      'https://www.serpong-garden-apartment.com/wp-content/uploads/sites/47/2019/04/serpong-garden-front-view.original.jpg',
+      125000000
+    ),
+    new Place(
+      'p3',
+      'BSD Apartment',
+      'Apartemen Murah',
+      'http://1.bp.blogspot.com/-tW_sDjrvzNA/Ueam7BtLbMI/AAAAAAAAAEc/lj1rW3TdaaM/s1600/casa-de-parco-apartment-bsd-city.jpg',
+      50000000
+    ),
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+  constructor() { }
+}
